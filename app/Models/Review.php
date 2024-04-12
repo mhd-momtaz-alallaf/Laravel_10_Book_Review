656cc6de7579aed221fb650648c90054ccacf5c6
Alallaf1 <parental.co122@gmail.com>
2024-04-12 16:11:20 +0300

    define the relation between Book and Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function books() // the reverse side of the relation between books and reviews in Book model.
+    {
+        return $this->belongsTo(Book::class); // one to one relation ( each review -> one book ).
+    }
 }
--- database/migrations/2024_04_12_121725_create_reviews_table.php
+++ database/migrations/2024_04_12_121725_create_reviews_table.php
@@ -13,11 +13,17 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-
+            
+            // $table->unsignedBigInteger('book_id');
             $table->text('review');
             $table->unsignedTinyInteger('rating');
 
             $table->timestamps();
+            // $table->foreign('book_id')->references('id')->on('books') // create a foreign key for "book_id" field of this table, refers to the primary key (id) on books table. 
+            //     ->onDelete('cascade');
+
+            $table->foreignId('book_id')->constrained() // this is an equevelant statment insted of tow lines above. (this used when we deeling with default tables feilds names (for primary key reference on the other table) ).
+                ->cascadeOnDelete();
         });
     }
 
