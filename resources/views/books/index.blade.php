309c6c344a9e35e6d821091745abb765f808cd24
Alallaf1 <parental.co122@gmail.com>
2024-04-13 12:32:50 +0300

    design the tabs of filtering books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -10,6 +10,26 @@
         <a href="{{route('books.index')}}" class="btn h-10" >Clear</a>
     </form>
 
+    <div class="filter-container mb-4 flex">
+        @php
+        $filters = [
+            '' => 'Latest',
+            'popular_last_month' => 'Popular Last Month',
+            'popular_last_6months' => 'Popular Last 6 Months',
+            'highest_rated_last_month' => 'Highest Rated Last Month',
+            'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+        ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', ['filter' => $key]) }}" 
+            class="{{ request('filter') === $key ? 'filter-item-active' : 'filter-item' }}">
+                {{$label}}
+            </a>    
+        @endforeach
+        
+    </div>
+
     <ul>
         @forelse ($books as $book)
             <li class="mb-4">
