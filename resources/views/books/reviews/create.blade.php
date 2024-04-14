3e255de1dc4a546f14f274e8eb5504785e1a63e4
Alallaf1 <parental.co122@gmail.com>
2024-04-14 14:08:35 +0300

    Revert "add errors to review create form"
    This reverts commit dda2488abf37a49ba983d3368dbd04388ea5bc06.

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -5,32 +5,20 @@
 
     <form  method="POST" action="{{route('books.reviews.store',$book)}}">
         @csrf
-        <div class="mb-2">
-            <label for="review">Review</label>
-            <textarea name="review" id="review" class="input mb-4"
-            @class(['border-red-500' => $errors->has('review')])
-            >{{$book->review ?? old('review')}}</textarea> <!-- id have to match "for" in label --> <!-- error'review' matchs the name attr-->
-            
-            @error('review')
-                <p class="error input mb-4">{{$message}}</p>
-            @enderror
-        </div>
-        <div class="mb-2">
-            <label for="rating">Rating</label>
-            <select name="rating" id="rating" class="input mb-4" 
-                @class(['border-red-500' => $errors->has('rating')]) >
-                <option value="">Select a Rating</option>
-                @for ($i = 0; $i <= 5; $i++)
-                    <option value="{{$i}}">{{$i}}</option>
-                @endfor
-            </select>
 
-            @error('rating')
-                <p class="error">{{ $message }}</p>
-            @enderror
-        </div>
+        <label for="review">Review</label>
+        <textarea name="review" id="review" class="input mb-4"
+        @class(['border-red-500' => $errors->has('review')])
+        >{{$book->review ?? old('review')}}</textarea> <!-- id have to match "for" in label --> <!-- error'review' matchs the name attr-->
+        
+        <label for="rating">Rating</label>
+        <select name="rating" id="rating" class="input mb-4" >
+            <option value="">Select a Rating</option>
+            @for ($i = 0; $i <= 5; $i++)
+                <option value="{{$i}}">{{$i}}</option>
+            @endfor
+        </select>
         <button type="submit" class="btn">Add Review</button>
     </form>
-
 
 @endsection
